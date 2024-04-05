8396785b723c3f082f8b54e8d2b527216823b5dc
Nabiyevv <nabiyevvm@gmail.com>
2024-04-05 14:53:56 +0400

    Add auto-animate plugin and animate.css

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -7,6 +7,8 @@ import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import App from './App.vue'
 import router from './router'
 import './echo.js';
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
+import 'animate.css';
 
 const app = createApp(App)
 
@@ -14,6 +16,7 @@ const app = createApp(App)
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
+app.use(autoAnimatePlugin)
 app.use(pinia)
 app.use(router)
 
